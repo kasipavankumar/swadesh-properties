076321ebde60d33f694f59572085e0143a788a3f
D. Kasi Pavan Kumar <44864604+kasipavankumar@users.noreply.github.com>
2021-09-29 14:28:14 +0530

    Remove layout component.

--- src/layouts/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <div className="min-h-screen flex flex-col" data-cy="layout">
-      {children}
-    </div>
-  );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/layouts/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: false,
   theme: {
